0b88917767354cc7c85a332154a7b0b7ee431726
Adriaan11 <gacroucamp@gmail.com>
2023-09-07 12:55:10 +0200

    Added empty BooksController

--- AdswerveMVCDemoAdriaanCroucamp/Data/ApplicationDbContext.cs
+++ AdswerveMVCDemoAdriaanCroucamp/Data/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using AdswerveMVCDemoAdriaanCroucamp.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace AdswerveMVCDemoAdriaanCroucamp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+
     }
 }
