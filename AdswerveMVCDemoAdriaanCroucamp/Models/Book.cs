6df2ce71e1ca81787c734ddd0650e94c042d6f4f
Adriaan11 <gacroucamp@gmail.com>
2023-09-07 12:50:21 +0200

    Added Folders and Model

---
+++ AdswerveMVCDemoAdriaanCroucamp/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdswerveMVCDemoAdriaanCroucamp.Models
+{
+    public class Book
+    {
+        public int Book_Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity_Available { get; set; }
+
+    }
+}
