fc9f8a5493b3b8f1b8238cd23c97fa49c32d4555
Adriaan11 <gacroucamp@gmail.com>
2023-09-07 13:46:38 +0200

    Added Comments

--- AdswerveMVCDemoAdriaanCroucamp/Controllers/BooksController.cs
+++ AdswerveMVCDemoAdriaanCroucamp/Controllers/BooksController.cs
@@ -17,9 +17,9 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Controllers
         // GET method for list/index
         public ActionResult Index()
         {
+            // Retrieve a list of books from the database
             var books = db.Books.ToList();
             return View(books);
-
         }
 
         // GET: Books/Details/5
@@ -27,35 +27,42 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                // Return a 400 Bad Request if 'id' is not provided
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // https://stackoverflow.com/questions/31287090/mvc-controller-return-a-bad-request
             }
 
+            // Find the book with the given 'id' in the database
             var book = db.Books.Find(id);
 
             if (book == null)
             {
+                // Return a 404 Not Found if the book with 'id' doesn't exist
                 return HttpNotFound();
             }
 
+            // Display details of the book
             return View(book);
         }
 
-
-        // GET method for edit button
+        // GET method for the edit page
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // https://stackoverflow.com/questions/31287090/mvc-controller-return-a-bad-request
+                // Return a 400 Bad Request if 'id' is not provided
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
+            // Find the book with the given 'id' in the database
             var book = db.Books.Find(id);
 
             if (book == null)
             {
+                // Return a 404 Not Found if the book with 'id' doesn't exist
                 return HttpNotFound();
             }
 
+            // Display the book for editing
             return View(book);
         }
 
@@ -66,11 +73,15 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Update the book's information in the database
                 db.Entry(book).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Details", new { id = book.Book_Id });
 
+                // Redirect to the details page of the edited book
+                return RedirectToAction("Details", new { id = book.Book_Id });
             }
+
+            // If there are validation errors, return to the edit page with error messages
             return View(book);
         }
 
@@ -79,49 +90,63 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Controllers
         {
             if (id == null)
             {
+                // Return a 400 Bad Request if 'id' is not provided
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
+            // Find the book with the given 'id' in the database
             var book = db.Books.Find(id);
 
             if (book == null)
             {
+                // Return a 404 Not Found if the book with 'id' doesn't exist
                 return HttpNotFound();
             }
 
+            // Display the book for deletion confirmation
             return View(book);
         }
 
+        // POST method to confirm book deletion
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+            // Find the book with the given 'id' in the database
             var book = db.Books.Find(id);
+
+            // Remove the book from the database
             db.Books.Remove(book);
             db.SaveChanges();
+
+            // Redirect to the book list after successful deletion
             return RedirectToAction("Index");
         }
 
         // GET: Books/Create
         public ActionResult Create()
         {
+            // Display the page to create a new book
             return View();
         }
 
-        // POST: Books/Create
+        // POST method to create a new book
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Book book)
         {
             if (ModelState.IsValid)
             {
+                // Add the new book to the database
                 db.Books.Add(book);
                 db.SaveChanges();
+
+                // Redirect to the book list after successful creation
                 return RedirectToAction("Index");
             }
+
+            // If there are validation errors, return to the create page with error messages
             return View(book);
         }
-
-
     }
 }
