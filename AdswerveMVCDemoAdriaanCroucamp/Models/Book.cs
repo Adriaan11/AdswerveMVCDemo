cbabd0f3d3c18683637c66be64b5901832845e63
Adriaan11 <gacroucamp@gmail.com>
2023-09-07 13:02:30 +0200

    Added data annotations

--- AdswerveMVCDemoAdriaanCroucamp/Models/Book.cs
+++ AdswerveMVCDemoAdriaanCroucamp/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,20 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Models
 {
     public class Book
     {
+        [Key]
+        [Column("book_id")]
         public int Book_Id { get; set; }
+        [Column("title")]
+
         public string Title { get; set; }
+        [Column("author")]
+
         public string Author { get; set; }
+        [Column("price")]
+
         public decimal Price { get; set; }
+        [Column("quantity_available")]
+
         public int Quantity_Available { get; set; }
 
     }
