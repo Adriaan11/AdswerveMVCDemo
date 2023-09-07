28b6cf5a2a863281d4c354b7402f11bc411004cc
Adriaan11 <gacroucamp@gmail.com>
2023-09-07 13:09:50 +0200

    Added Get Method in controller + Edit View

--- AdswerveMVCDemoAdriaanCroucamp/Controllers/BooksController.cs
+++ AdswerveMVCDemoAdriaanCroucamp/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using AdswerveMVCDemoAdriaanCroucamp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +19,24 @@ namespace AdswerveMVCDemoAdriaanCroucamp.Controllers
             var books = db.Books.ToList();
             return View(books);
 
+        }
+
+        // GET method for edit button
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // https://stackoverflow.com/questions/31287090/mvc-controller-return-a-bad-request
+            }
+
+            var book = db.Books.Find(id);
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
         }
     }
 }
